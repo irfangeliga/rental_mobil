5e1bbc3641892892a492dfe5e2c123d0f9d72484
irfangeliga <muhammad.2960@students.amikom.ac.id>
2023-07-17 09:51:44 +0700

    menambahkan file awal

---
+++ rest/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$hostname = "localhost";
+$database = "mobil";
+$username = "root";
+$password = "!!";
+$connect = mysqli_connect($hostname, $username, $password, $database);
+if (!$connect) {
+    die("Koneksi Tidak Berhasil: " . mysqli_connect_error());
+}
